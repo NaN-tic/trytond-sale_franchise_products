aaa10b5e101630ee8b38ecf9f56579f6705e42f0
Guillem Barba <guillem@nan-tic.com>
2016-03-29 15:29:49 +0200

    Improved franchise views

--- product.py
+++ product.py
@@ -1,7 +1,5 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from decimal import Decimal
-
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
