e2313ee1cd4faa25622bbf9890470e433998f47e
?ngel ?lvarez <angel@nan-tic.com>
2018-10-11 11:04:23 +0200

    update to 4.7

--- sale.py
+++ sale.py
@@ -1,4 +1,4 @@
-h# The COPYRIGHT file at the top level of this repository contains
+# The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from collections import defaultdict
 
